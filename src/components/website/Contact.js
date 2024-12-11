b7f613c44fa6715ca3aa9654fa041708a2f27db7
prafull sapkal <prafullsapkal2005@gmail.com>
2024-12-11 17:27:12 +0530

    css change on demand

--- src/components/website/Contact.js
+++ src/components/website/Contact.js
@@ -23,87 +23,85 @@ const Contact = () => {
 
 
   return (
-     
-        
-       
-        <div class="container pt-10">
-          <span class="big-circle"></span>
-          <img src="img/shape.png" class="square" alt="" />
-          <div class="form">
-            <div class="contact-info">
-              <h3 class="title">Let's get in touch</h3>
-              <p class="text">
-                 Book your Cab With full Safety And Enjoy 
-              </p>
 
-              <div class="info">
-                <div class="information">
-                  <i class="fas fa-map-marker-alt"></i> Our location :
+    <div class="contact container pt-10">
+      <span class="big-circle"></span>
+      <img src="img/shape.png" class="square" alt="" />
+      <div class="form">
+        <div class="contact-info">
+          <h3 class="title">Let's get in touch</h3>
+          <p class="text">
+            Book your Cab With full Safety And Enjoy
+          </p>
 
-                  <p>Chhatrapati Sambhajinagar (Aurangabad), Maharashtra 431003</p>
-                </div>
-                <div class="information">
-                  <i class="fas fa-envelope"></i> Email Adress :
-                  <p>info@ewebdigital.com</p>
-                </div>
-                <div class="information">
-                  <i class="fas fa-phone"></i> Phone Number :
-                  <p>+91 8983848591</p>
-                </div>
-              </div>
+          <div class="info">
+            <div class="information">
+              <i class="fas fa-map-marker-alt"></i> Our location :
 
-              <div class="social-media">
-                <p>Connect with us :</p>
-                <div class="social-icons">
-                  <a href="#">
-                    <i class="fab fa-facebook-f"></i>
-                  </a>
-                  <a href="#">
-                    <i class="fab fa-twitter"></i>
-                  </a>
-                  <a href="#">
-                    <i class="fab fa-instagram"></i>
-                  </a>
-                  <a href="#">
-                    <i class="fab fa-linkedin-in"></i>
-                  </a>
-                </div>
-              </div>
+              <p>Chhatrapati Sambhajinagar (Aurangabad), Maharashtra 431003</p>
             </div>
+            <div class="information">
+              <i class="fas fa-envelope"></i> Email Adress :
+              <p>info@ewebdigital.com</p>
+            </div>
+            <div class="information">
+              <i class="fas fa-phone"></i> Phone Number :
+              <p>+91 8983848591</p>
+            </div>
+          </div>
 
-            <div class="contact-form">
-              <span class="circle one"></span>
-              <span class="circle two"></span>
-
-              <form action="index.html" autocomplete="off">
-                <h3 class="title">Contact us</h3>
-                <div class="input-container">
-                  <input type="text" name="name" class="input" />
-                  <label for="">Username</label>
-                  <span>Username</span>
-                </div>
-                <div class="input-container">
-                  <input type="email" name="email" class="input" />
-                  <label for="">Email</label>
-                  <span>Email</span>
-                </div>
-                <div class="input-container">
-                  <input type="tel" name="phone" class="input" />
-                  <label for="">Phone</label>
-                  <span>Phone</span>
-                </div>
-                <div class="input-container textarea">
-                  <textarea name="message" class="input"></textarea>
-                  <label for="">Message</label>
-                  <span>Message</span>
-                </div>
-                <input type="submit" value="Send" class="btn" />
-              </form>
+          <div class="social-media">
+            <p>Connect with us :</p>
+            <div class="social-icons">
+              <a href="#">
+                <i class="fab fa-facebook-f"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-twitter"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <a href="#">
+                <i class="fab fa-linkedin-in"></i>
+              </a>
             </div>
           </div>
         </div>
 
-         
+        <div class="contact-form">
+          <span class="circle one"></span>
+          <span class="circle two"></span>
+
+          <form action="index.html" autocomplete="off">
+            <h3 class="title">Contact us</h3>
+            <div class="input-container">
+              <input type="text" name="name" class="input" />
+              <label for="">Username</label>
+              <span>Username</span>
+            </div>
+            <div class="input-container">
+              <input type="email" name="email" class="input" />
+              <label for="">Email</label>
+              <span>Email</span>
+            </div>
+            <div class="input-container">
+              <input type="tel" name="phone" class="input" />
+              <label for="">Phone</label>
+              <span>Phone</span>
+            </div>
+            <div class="input-container textarea">
+              <textarea name="message" class="input"></textarea>
+              <label for="">Message</label>
+              <span>Message</span>
+            </div>
+            <input type="submit" value="Send" class="btn" />
+          </form>
+        </div>
+      </div>
+    </div>
+
+
   )
 }
 

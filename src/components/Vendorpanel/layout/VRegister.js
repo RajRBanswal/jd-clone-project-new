abf61a60b0ed9bd05fcd441db77db32994628da1
Rohan Nagpure <rohannagpure2022@gmail.com>
2024-12-13 17:42:35 +0530

    design admin & vendor login or signup form

--- src/components/Vendorpanel/layout/VRegister.js
+++ src/components/Vendorpanel/layout/VRegister.js
@@ -2,37 +2,60 @@ import React from 'react'
 
 const VRegister = () => {
     return (
-        <div class="container-fluid">
+        <div class="container-fluid text-center">
             <div class="row h-100 align-items-center justify-content-center">
-                <div class="col-12 col-sm-8 col-md-6 col-lg-5 col-xl-4">
+                <div class=" col-sm-6 m-auto mx-t3 mb-4 ">
                     <div class="bg-light rounded p-4 p-sm-5 my-4 mx-3">
-                        <div class="d-flex align-items-center justify-content-between mb-3">
-                            <a href="index.html" class="">
-                                <h3 class="text-primary"><i class="fa fa-hashtag me-2"></i>VENDOR-sign up</h3>
-                            </a>
-                            <h3>Sign Up</h3>
+                        <a href="index.html" class="text-center">
+                            <h3 class="text-primary text-center">VENDOR SignUp</h3>
+                        </a>
+
+
+                        <div class="information-form mt-3">
+                            <div class="row">
+                                <div class="form-floating mb-3  col-sm-6">
+                                    <input type="text" class="form-control" id="floatingText" placeholder="jhondoe" />
+                                    <label for="floatingText"> First Name</label>
+                                </div>
+                                <div class="form-floating mb-3 col-sm-6">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                                    <label for="floatingInput">Last Name</label>
+                                </div>
+                            </div>
+
+
+
+                            <div class="row">
+                                <div class="form-floating mb-3 col-sm-6">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                                    <label for="floatingInput"> Phone No</label>
+                                </div>
+                                <div class="form-floating mb-3 col-sm-6">
+                                    <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
+                                    <label for="floatingInput">Email address</label>
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="form-floating mb-3 col-sm-6">
+                                    <input type="password" class="form-control" id="floatingInput" placeholder="password" />
+                                    <label for="password"> Password</label>
+                                </div>
+                                <div class="form-floating mb-4 col-sm-6">
+                                    <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
+                                    <label for="floatingPassword"> Confirm Password</label>
+                                </div>
+                            </div>
                         </div>
-                        <div class="form-floating mb-3">
-                            <input type="text" class="form-control" id="floatingText" placeholder="jhondoe" />
-                            <label for="floatingText">Username</label>
-                        </div>
-                        <div class="form-floating mb-3">
-                            <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
-                            <label for="floatingInput">Email address</label>
-                        </div>
-                        <div class="form-floating mb-4">
-                            <input type="password" class="form-control" id="floatingPassword" placeholder="Password" />
-                            <label for="floatingPassword">Password</label>
-                        </div>
+
                         <div class="d-flex align-items-center justify-content-between mb-4">
                             <div class="form-check">
                                 <input type="checkbox" class="form-check-input" id="exampleCheck1" />
                                 <label class="form-check-label" for="exampleCheck1">Check me out</label>
-                            </div>
-                            <a href="">Forgot Password</a>
+                            </div> 
                         </div>
-                        <button type="submit" class="btn btn-primary py-3 w-100 mb-4">Sign Up</button>
-                        <p class="text-center mb-0">Already have an Account? <a href="">Sign In</a></p>
+                        <button type="submit" class="btn btn-primary py-3 w-25 mb-4">Sign Up</button>
+                        <p class="text-center mb-0">Already have an Account? <a href="vendor-login">Sign In</a></p>
                     </div>
                 </div>
             </div>

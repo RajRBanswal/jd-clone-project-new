9f91eb966e2ca1eb9076d4056043f2494d4925f7
Tushar Sapkal <tusharsapkal2004@gmail.com>
2024-12-28 14:08:50 +0530

    create responsive and stylish  vendor login and registration forms

--- src/components/Vendorpanel/Vendorlogin.js
+++ src/components/Vendorpanel/Vendorlogin.js
@@ -3,37 +3,43 @@ import React from 'react'
 const Vendorlogin = () => {
   return (
     <div class="wrapper">
-      <div class=" ven-login container">
-        <div class="col-left">
-          <div class="login-text">
-            <h2>Welcome Back</h2>
-            
-            <a class="btn" href="">Sign Up</a>
-          </div>
+    <div class="registration-container">
+      <div class="col-left">
+        <div class="info-box">
+          <h2>Welcome Back</h2>
+          <p>Join our platform to unlock exclusive benefits and opportunities for your business.</p>
+          <a class="btn" href="/vender-signup">Create An acoout? Sign up</a>
+          
         </div>
-        <div class="col-right">
-          <div class="login-form">
-            <h2>Vendor-Login</h2>
-            <form>
-              <p>
-                <label>Username or email address<span>*</span></label>
-                <input type="text" placeholder="Username or Email" required />
-              </p>
-              <p>
-                <label>Password<span>*</span></label>
-                <input type="password" placeholder="Password" required />
-              </p>
-              <p>
-                <input type="submit" value="Sing In" />
-              </p>
-              <p>
-                <a class="mx-5" href="">Forget Password?</a>
-              </p>
-            </form>
+      </div>
+      <div class="col-right">
+        <div class="form-box">
+          <h2>Sign In Your Vendor Account</h2>
+          <form>
+            <div class="form-group">
+              <label>Email / mobile.No<span>*</span></label>
+              <input type="text" placeholder="Enter your Email / Mobile.No" required />
+            </div>
+            
+            <div class="form-group">
+              <label>Password<span>*</span></label>
+              <input type="password" placeholder="Enter your password" required />
+            </div>
+             
+            <button type="submit" class="submit-btn">Sign In</button>
+            <div class="form-footer">
+            <a href="#">Forgot Password?</a> 
           </div>
+          </form>
+          
         </div>
       </div>
     </div>
+  </div>
+  
+
+
+
   )
 }
 

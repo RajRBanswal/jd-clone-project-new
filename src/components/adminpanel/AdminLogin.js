17d5a609f89b56e34930d7dc8d87c5ef970dc6a9
Rohan Nagpure <rohannagpure2022@gmail.com>
2024-12-28 17:28:15 +0530

    create stylish login form

--- src/components/adminpanel/AdminLogin.js
+++ src/components/adminpanel/AdminLogin.js
@@ -2,39 +2,39 @@ import React from 'react'
 
 const AdminLogin = () => {
     return (
-      <div class="wrapper">
-      <div class=" ven-login container">
-        <div class="col-left">
-          <div class="login-text">
-            <h2>Welcome Back</h2>
-            
-             
-          </div>
-        </div>
-        <div class="col-right">
-          <div class="login-form">
-            <h2>Admin-Login</h2>
-            <form>
-              <p>
-                <label>Username or email address<span>*</span></label>
-                <input type="text" placeholder="Username or Email" required />
-              </p>
-              <p>
-                <label>Password<span>*</span></label>
-                <input type="password" placeholder="Password" required />
-              </p>
-              <p>
-                <input type="submit" value="Sing In" />
-              </p>
-              <p>
-                <a class ="mx-5" href="">Forget Password?</a>
-              </p>
-            </form>
-          </div>
-        </div>
+      <div class="d-flex align-items-center justify-content-center vh-100">
+    <div class="login-card">
+      <div class="text-center">
+        <h3>Admin Login</h3>
+        <p>Access your admin account securely</p>
       </div>
+      <form>
+        <div class="mb-3">
+          <input
+            type="text"
+            class="form-control"
+            placeholder="Username"
+            required
+          />
+        </div>
+        <div class="mb-3">
+          <input
+            type="password"
+            class="form-control"
+            placeholder="Password"
+            required
+          />
+        </div>
+        <div class="d-grid">
+          <button class="login-log" type="submit">Login</button>
+        </div>
+        <div class="forgot-link">
+          <a href="#">Forgot Password?</a>
+        </div>
+      </form>
     </div>
-
+  </div>
+    
     );
 };
 

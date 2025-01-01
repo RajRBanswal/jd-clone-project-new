bddacb80b2d5b41d7a67acdb63663bc5b1e6991d
Tushar Sapkal <tusharsapkal2004@gmail.com>
2025-01-01 09:57:39 +0530

    login

--- src/components/website/Login.js
+++ src/components/website/Login.js
@@ -1,7 +1,43 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { useState,useEffect} from 'react'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+
+  const navigate = useNavigate()
+
+  const userName = localStorage.getItem('userUsername')
+  console.log(userName);
+
+ useEffect(() => {
+    if (userName !== null) {
+      navigate("/")
+    }
+  }, []); 
+
+  const [Email, setEmail] = useState("")
+  const [Password, setPassword] = useState("")
+  const getLogin = async () => {
+    const response = await fetch("http://localhost:8000/api/user-login", {
+      method: "POST",
+      body: JSON.stringify({
+        Email,
+        Password
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    const result = await response.json();
+    console.log(result);
+    
+    if (result.status === 200) {
+      alert(result.message);
+      navigate("/")
+    } else {
+      alert(result.message);
+    }
+  }
   return (
     <div class="container d-flex align-items-center justify-content-center vh-100">
     <div class="login-card">
@@ -10,13 +46,13 @@ const Login = () => {
       <p>Access your account</p>
       <form>
         <div class="mb-3">
-          <input type="text" class="form-control" placeholder="Email or Username" required/>
+          <input type="text" class="form-control" placeholder="Email"  onChange={(e) => setEmail(e.target.value)} required/>
         </div>
         <div class="mb-3">
-          <input type="password" class="form-control" placeholder="Password" required/>
+          <input type="password" class="form-control" placeholder="Password" onChange={(e) => setPassword(e.target.value)} required/>
         </div>
         <div class="d-grid">
-          <button type="submit" class="btn btn-primary">Login</button>
+          <button type="button" onClick={getLogin} class="btn btn-primary">Login</button>
         </div>
         <div class="forgot-link mt-3">
           <a href="#">Forgot Password?</a> / <a href="/signup">Signup</a>
--- src/components/website/Signup.js
+++ src/components/website/Signup.js
@@ -1,6 +1,43 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = () => {
+
+  const navigate = useNavigate();
+
+
+  const [name, setname] = useState("")
+  const [mobile, setmobile] = useState("")
+  const [email, setemail] = useState("")
+  const [password, setpassword] = useState("")
+
+  const getRegister = async () => {
+    const response = await fetch("http://localhost:8000/api/user-registration", {
+      method: "POST",
+      body: JSON.stringify({
+        email,
+        name,
+        password,
+        mobile
+      }),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+
+    const result = await response.json();
+    console.log(result);
+
+    if (result.status === 200) {
+      alert(result.message);
+      navigate("/login")
+    } else {
+      alert(result.message);
+    }
+
+  }
+
     return (
         <div class="wrapper">
         <div class="registration-container">
@@ -17,25 +54,25 @@ const Signup = () => {
               <form>
                 <div class="form-group">
                   <label>Name<span>*</span></label>
-                  <input type="text" placeholder="Enter your Name" required />
+                  <input type="text" placeholder="Enter your Name" onChange={(e) => setname(e.target.value)}  required />
                 </div>
                 <div class="form-group">
                   <label>Mobile.No<span>*</span></label>
-                  <input type="number" placeholder="Enter your Mobile.No" required />
+                  <input type="number" placeholder="Enter your Mobile.No" onChange={(e) => setmobile(e.target.value)}  required />
                 </div>
                 <div class="form-group">
                   <label>Email<span>*</span></label>
-                  <input type="email" placeholder="Enter your Email" required />
+                  <input type="email" placeholder="Enter your Email" onChange={(e) => setemail(e.target.value)}  required />
                 </div>
                 <div class="form-group">
                   <label>Password<span>*</span></label>
-                  <input type="password" placeholder="Enter your password" required />
+                  <input type="password" placeholder="Enter your password" onChange={(e) => setpassword(e.target.value)}  required />
                 </div>
                 <div class="form-group">
                   <label>Confirm Password<span>*</span></label>
-                  <input type="password" placeholder="Re-enter your password" required />
+                  <input type="password" placeholder="Re-enter your password" onChange={(e) => setpassword(e.target.value)}  required />
                 </div>
-                <button type="submit" class="submit-btn">Register</button>
+                <button type="button" onClick={getRegister} class="submit-btn">Register</button>
               </form>
             </div>
           </div>
